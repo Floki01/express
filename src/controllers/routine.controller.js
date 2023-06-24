1966f178338114a0c2f8ea09315f327a2940066f
Claudio Saez <c.saez15@ufromail.cl>
2023-06-23 22:23:22 -0400

    Se corrigen las funciones de eliminar-listar

--- src/controllers/routine.controller.js
+++ src/controllers/routine.controller.js
@@ -1,5 +1,5 @@
 import routineModel from "../models/routine.model.js";
-
+import userModel from "../models/user.model.js"
 
 export async function eliminarRutina(req, res) {
     const idRutina = req.params.idRutina;
@@ -8,11 +8,10 @@ export async function eliminarRutina(req, res) {
         // Buscar la rutina por su ID
         const rutina = await routineModel.findById(idRutina);
         if (!rutina) {
-           
             return res.status(404).send({ message: 'Rutina no encontrada' });
         }
         // Eliminar la rutina de la base de datos
-        await rutina.remove();
+        await rutina.deleteOne();
         return res.status(200).send({ message: 'Rutina eliminada correctamente' });
     } catch (error) {
         return res.status(500).send({ error });
@@ -22,18 +21,13 @@ export async function eliminarRutina(req, res) {
 
 export async function getRutinasPorUsuario(req, res) {
     const idUsuario = req.params.idUsuario;
-
+  
     try {
-        // Buscar el usuario por su ID y obtener las rutinas asociadas
-        const usuario = await userModel.findById(idUsuario).populate('routines');
+        const usuario = await userModel.findById(idUsuario);
+        const rutinas = await routineModel.find({user: usuario._id});
         if (!usuario) {
             return res.status(404).send({ message: 'Usuario no encontrado' });
         }
-        // Mapear las rutinas del usuario y agregar los ejercicios asociados a cada rutina
-        const rutinas = usuario.routines.map(routine => ({
-            ...routine._doc,
-            exercises: routine.getExercises(),
-        }));
         return res.status(200).send({ rutinas });
     } catch (error) {
         return res.status(500).send({ error });
