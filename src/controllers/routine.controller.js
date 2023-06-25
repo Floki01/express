c75c1ba43dcba061a827da98f1e2c9392e3112a8
Claudio Saez <c.saez15@ufromail.cl>
2023-06-24 23:40:33 -0400

    mejora del eliminar-rutina

--- src/controllers/routine.controller.js
+++ src/controllers/routine.controller.js
@@ -1,21 +1,30 @@
 import routineModel from "../models/routine.model.js";
 import userModel from "../models/user.model.js"
+import exerciseModel from "../models/exercise.model.js";
 
 export async function eliminarRutina(req, res) {
     const idRutina = req.params.idRutina;
 
     try {
         // Buscar la rutina por su ID
-        const rutina = await routineModel.findById(idRutina);
+        const rutina = await routineModel.findById(idRutina).populate("exercises");
         if (!rutina) {
-            return res.status(404).send({ message: 'Rutina no encontrada' });
+          return res.status(404).send({ message: 'Rutina no encontrada' });
         }
+    
+        // para eliminar ejercicios de las rutinas
+        const ejercicios = rutina.exercises;
+        for (const ejercicio of ejercicios) {
+          await exerciseModel.findByIdAndDelete(ejercicio._id);
+        }
+    
         // Eliminar la rutina de la base de datos
         await rutina.deleteOne();
+    
         return res.status(200).send({ message: 'Rutina eliminada correctamente' });
-    } catch (error) {
+      } catch (error) {
         return res.status(500).send({ error });
-    }
+      }
 }
 
 
